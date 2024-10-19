11e2c95bc9877faaca285c3587cf09fbd900f315
fatma ahmed <fa565221@gmail.com>
2024-10-19 20:48:14 +0300

    first commit

---
+++ src/main/java/com/fatma/Leader_Acadmy/LeaderAcadmyApplication.java
@@ -0,0 +1,13 @@
+package com.fatma.Leader_Acadmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaderAcadmyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaderAcadmyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatma/Leader_Acadmy/LeaderAcadmyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatma.Leader_Acadmy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderAcadmyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
